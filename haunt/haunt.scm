1cb793f65bcb53f9cde007cd885d53201a3c57eb
SCP-EEE <scpeee@wail.ch>
2022-07-19 08:26:09 +0900

    haunt

---
+++ haunt/haunt.scm
@@ -0,0 +1,21 @@
+(use-modules (haunt asset)
+             (haunt builder blog)
+             (haunt builder atom)
+             (haunt builder assets)
+             (haunt builder rss)
+             (haunt reader)
+             (haunt reader texinfo)
+             (haunt reader commonmark)
+             (haunt site))
+
+(site #:title "ようこそ"
+      #:domain "nodokaha.github.io/SCP-EEE.github.io"
+      #:default-metadata
+      '((author . "SCP-EEE")
+        (email  . "scpeee@wail.ch"))
+      #:readers (list texinfo-reader sxml-reader html-reader commonmark-reader)
+      #:builders (list (blog)
+                       (atom-feed)
+                       (atom-feeds-by-tag)
+                       (rss-feed)
+                       (static-directory "images")))
