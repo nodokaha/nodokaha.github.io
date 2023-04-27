252077e42f534a6b971398050208885886bbe21a
nodokaha <scpeee@wail.ch>
2023-04-27 18:23:27 +0900

    update pwn

--- chall.c
+++ chall.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void win(){printf("congratulation!!\nHere flag!\nTKCTF{%c%c%c%c%c%c%c}", 0x59, 0x6f, 0x75, 0x5f, 0x70, 0x77, 0x6e);}
+
+void a(){}
+
 int main(void){
   struct keypair{
     char name[100];
@@ -11,9 +15,13 @@ int main(void){
   scanf("%s", keypair.name);
   printf("OK! Hello %s, Please, key!\n", keypair.name);
   scanf("%d", &keypair.key);
-  if(keypair.key == 65537 || keypair.admin == 1)
+  if(keypair.key == 65537)
     printf("congratulation!!\nHere flag!\nTKCTF{%c%c%c%c%c%c%c}", 0x59, 0x6f, 0x75, 0x5f, 0x70, 0x77, 0x6e);
   else
     printf("Oops...You are not hacker.....");
+  void (*fun)() = &a;
+  if(keypair.admin)
+    fun = &win;
+  fun();
   return 0;
 }
